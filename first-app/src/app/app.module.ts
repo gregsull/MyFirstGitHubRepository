1989caf39f25da995b1bc2ce7d96f6b92da2889d
Greg Sullivan <gregsull@cox.net>
2019-12-06 22:04:56 -0700

    Added some tutorial exercise code

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RedComponent } from './red/red.component';
+import { GreenComponent } from './green/green.component';
+import { YellowComponent } from './yellow/yellow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedComponent,
+    GreenComponent,
+    YellowComponent
   ],
   imports: [
     BrowserModule
